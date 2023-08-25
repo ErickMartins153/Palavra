91d76f6dc5c04244d83a96dbe92abcf116e918a4
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-08-24 21:37:28 -0300

    insert user input into div

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const wordsDashboard = document.querySelector(".words-dashboard");
+
+let count = 1;
 for (let i = 0; i < 30; i++) {
   const letterDiv = document.createElement("div");
-  letterDiv.classList.add(`letter-${i}`);
-  letterDiv.textContent = i;
+  letterDiv.classList.add(`letter`);
   wordsDashboard.appendChild(letterDiv);
 }
+
+document.onkeyup = function (e) {
+  const letterSpaces = document.querySelectorAll(".letter");
+  if (count > letterSpaces.length) return;
+  for (let i = 0; i < count; i++) {
+    if (!letterSpaces[i].textContent) {
+      letterSpaces[i].textContent = e.key.toUpperCase();
+    }
+  }
+  count++;
+};
