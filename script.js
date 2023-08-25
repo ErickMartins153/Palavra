92c77956c8819b8e28895b27b879c789c275d49f
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-08-24 22:41:14 -0300

    input only one row per submit

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ const wordsDashboard = document.querySelector(".words-dashboard");
 let count = 1;
 for (let i = 0; i < 30; i++) {
   const letterDiv = document.createElement("div");
-  letterDiv.classList.add(`letter`);
+  letterDiv.classList.add("letter");
   wordsDashboard.appendChild(letterDiv);
 }
 
 document.onkeyup = function (e) {
+  if (!isLetter(e.key) && e.key !== "Backspace") return;
   const letterSpaces = document.querySelectorAll(".letter");
-  if (count > letterSpaces.length) return;
+  if (count > 5) {
+    count = 5;
+  }
   for (let i = 0; i < count; i++) {
-    if (!letterSpaces[i].textContent) {
+    if (e.key === "Backspace") {
+      count--;
+      if (count < 1) count = 0;
+      letterSpaces[count].textContent = null;
+      return;
+    } else if (!letterSpaces[i].textContent) {
       letterSpaces[i].textContent = e.key.toUpperCase();
     }
   }
   count++;
 };
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
